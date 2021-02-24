cd42399c97e330d61ac8f4d9768c9415e964c20b
aliraza1 <contactaz@gmail.com>
2021-02-24 00:12:42 -0500

    commiting pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,24 +1,29 @@
 pipeline {
     agent any
-
-   
+    triggers { pollSCM('* * * * *') }
     stages {
         stage('Checkout') {
-           steps {
-                git url: 'https://github.com/aliraza1/jgsu-spring-petclinic.git', branch: 'main'
-            }
+            steps {
+                git url: 'https://github.com/aliraza1/jgsu-spring-petclinic.git', branch: 'main'            }            
         }
         stage('Build') {
             steps {
                 sh './mvnw clean package'
+                //sh 'false' // true
             }
-
-           post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                   junit '**/target/surefire-reports/TEST-*.xml'
+        
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                }
+                changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build", 
+                        attachLog: true, 
+                        compressLog: true, 
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
